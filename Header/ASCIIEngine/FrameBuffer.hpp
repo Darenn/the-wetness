c0848d4249b2aaa6adf3389427ddec9425c79346
Aredhele <vincentstehly@hotmail.fr>
2017-10-03 10:41:46 +0200

    Add new sources before refactoring

---
+++ Header/ASCIIEngine/FrameBuffer.hpp
@@ -0,0 +1,22 @@
+/// \file       FrameBuffer.hpp
+/// \date       03/10/2017
+/// \package    ASCIIENgine
+/// \author     Vincent STEHLY--CALISTO
+
+#ifndef _FRAME_BUFFER_HPP
+#define _FRAME_BUFFER_HPP
+
+/// \class  FrameBuffer
+/// \brief  Stores a frame 
+class FrameBuffer
+{
+public:
+
+
+
+
+private:
+
+};
+
+#endif // _FRAME_BUFFER_HPP
