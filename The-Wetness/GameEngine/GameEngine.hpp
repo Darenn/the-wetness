296292c5f2e1aca2b68ee3e0fe2bf1e377049ac6
Aredhele <vincentstehly@hotmail.fr>
2017-10-08 14:48:15 +0200

    Added game engine initialization in main

--- The-Wetness/GameEngine/GameEngine.hpp
+++ The-Wetness/GameEngine/GameEngine.hpp
@@ -6,7 +6,15 @@
 #ifndef _GAME_ENGINE_HPP
 #define _GAME_ENGINE_HPP
 
+#include <string>
+
 #include "Timer.hpp"
+#include "Vector2.hpp"
+
+#define GAME_ENGINE_MIN_FPS 30
+#define GAME_ENGINE_MAX_FPS 240
+#define GAME_ENGINE_MIN_UPS 60
+#define GAME_ENGINE_MAX_UPS 240
 
 /// \class  GameEngine
 /// \brief  Handles rendering, FPS etc.
@@ -16,12 +24,22 @@ public:
 
 	explicit GameEngine(void);
 
-	void Initialize();
-	void Start();
+	void SetInitDebug       (bool debug);
+	void SetInitFrameRate	(float fps);
+	void SetInitUpdateRate  (unsigned int ups);
+	void SetInitWindowTitle	(const char * pTitle);
+	void SetInitWindowSize	(const Vector2u & size);
+
+	void Initialize	(void);
+	void Start		(void);
 
 private:
 
-	// TODO
+	bool		m_debug;
+	float       m_fps;
+	float       m_granularity;
+	Vector2u	m_windowSize;
+	std::string m_windowTitle;
 };
 
 #endif // _GAME_ENGINE_HPP
