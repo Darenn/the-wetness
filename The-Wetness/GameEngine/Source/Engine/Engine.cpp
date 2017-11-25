688da27bab62d31a23f03089a0760ff1df1371dc
Aredhele <vincentstehly@hotmail.fr>
2017-11-25 16:34:15 +0100

    Adds engine user interface

---
+++ The-Wetness/GameEngine/Source/Engine/Engine.cpp
@@ -0,0 +1,9 @@
+/// \file       Engine.cpp
+/// \date       25/11/2017
+/// \package    Engine
+/// \author     Vincent STEHLY--CALISTO
+
+#include "Engine/Engine.hpp"
+
+/* static */ RenderingEngine * const Engine::pRendering    = new RenderingEngine();
+/* static */ StateMachine    * const Engine::pStateMachine = new StateMachine();
---
+++ The-Wetness/GameEngine/Source/Game/State/GameState.cpp
@@ -0,0 +1,26 @@
+/// \file       GameState.cpp
+/// \date       25/11/2017
+/// \package    Game
+/// \author     Vincent STEHLY--CALISTO
+
+#include "Engine/Engine.hpp"
+#include "Game/State/GameState.hpp"
+
+/// \brief	Called when the state is pushed
+void GameState::OnEnter(void)
+{
+	// TODO
+}
+
+/// \brief	Called when the state is removed
+void GameState::OnExit(void)
+{
+	// TODO
+}
+
+/// \brief	Called each frame
+/// \param  dt The elapsed time since the last frame
+void GameState::Update(float dt)
+{
+	// TODO
+}
