99e63cbdf5040299ebaa34051df5f24a0ef5c8e8
Aredhele <vincentstehly@hotmail.fr>
2017-11-26 19:06:39 +0100

    Optimized Draw(grid) and added doc

--- The-Wetness/Source/Engine/Rendering/RenderingEngineDraw.cpp
+++ The-Wetness/Source/Engine/Rendering/RenderingEngineDraw.cpp
@@ -7,37 +7,61 @@
 #include "Engine/Rendering/RectangleShape.hpp"
 #include "Engine/Rendering/RenderingEngine.hpp"
 
+/// \brief	Draws a wchar at the given position
+/// \param  value The wchar to draw
+/// \param  attribute The attribute of the wchar (color)
+/// \param  x The x coordinate
+/// \param  y The y coordinate
 void RenderingEngine::Draw(WCHAR value, WORD attribute, USHORT x, USHORT y)
 {
 	m_window.Draw(value, attribute, x, y);
 }
 
+/// \brief	Draws a char at the given position
+/// \param  value The char to draw
+/// \param  attribute The attribute of the char (color)
+/// \param  x The x coordinate
+/// \param  y The y coordinate
 void RenderingEngine::Draw(CHAR  value, WORD attribute, USHORT x, USHORT y)
 {
 	m_window.Draw(value, attribute, x, y);
 }
 
+/// \brief	Draws a char info buffer a the given position
+/// \param  pBuffer The char to draw
+/// \param  h The height of the buffer
+/// \param  w The width of the buffer
+/// \param  x The x coordinate
+/// \param  y The y coordinate
 void RenderingEngine::Draw(CHAR_INFO * pBuffer, USHORT h, USHORT w, USHORT x, USHORT y)
 {
 	m_window.Draw(pBuffer, h, w, x, y);
 }
 
-// Other draw methods
+/// \brief	Draws a string at the given position
+/// \param  text The string to draw
+/// \param  x The x coordinate
+/// \param  y The y coordinate
 void RenderingEngine::Draw(std::string const& text, USHORT x, USHORT y)
 {
 	Draw(text.c_str(), x, y);
 }
 
+/// \brief	Draws a string at the given position
+/// \param  text The c string to draw
+/// \param  x The x coordinate
+/// \param  y The y coordinate
 void RenderingEngine::Draw(char const * text, USHORT x, USHORT y)
 {
 	while (*text)
 	{
 		m_window.Draw(*text, 0x0F, x, y);
-		++x;
-		++text;
+		++text; ++x;
 	}
 }
 
+/// \brief	Draws a rectangle shape at the given position
+/// \param  rect The rectangle shape to draw
 void RenderingEngine::Draw(RectangleShape const& rect)
 {
 	for (USHORT h = 0; h < rect.h; ++h)
@@ -49,6 +73,9 @@ void RenderingEngine::Draw(RectangleShape const& rect)
 	}
 }
 
+/// \brief	Draws the grid at the center of the screen
+/// \param  formatedGrid The grid as a string
+/// \param  gridSize The size of the grid
 void RenderingEngine::Draw(std::string const& formatedGrid, Vector2u const& gridSize)
 {
 	USHORT blockSize = 15;
@@ -58,27 +85,22 @@ void RenderingEngine::Draw(std::string const& formatedGrid, Vector2u const& grid
 
 	USHORT width  = gridSize.x * 2;
 	USHORT height = gridSize.y * 2;
-	USHORT puzzleWidth  = width * blockSize;
+	USHORT puzzleWidth  = width  * blockSize;
 	USHORT puzzleHeight = height * blockSize;
 
 	USHORT centerX = m_window.GetWidth()  / 2;
 	USHORT centerY = m_window.GetHeight() / 2;
-
-	USHORT startX = centerX - puzzleWidth;
-	USHORT startY = centerY - puzzleHeight;
+	USHORT startX  = centerX - puzzleWidth;
+	USHORT startY  = centerY - puzzleHeight;
 
 	// Draws grey borders
-    Draw(RectangleShape(
-		startX - borderThickness,
-		startY - borderThickness,
+    Draw(RectangleShape(startX - borderThickness, startY - borderThickness,
 		puzzleHeight + borderThickness,
 		puzzleWidth  + borderThickness,
 		Color::Grey));
 
 	// Draws puzzle background
-	Draw(RectangleShape(
-		startX - playGroundThickness,
-		startY - playGroundThickness,
+	Draw(RectangleShape(startX - playGroundThickness, startY - playGroundThickness,
 		puzzleHeight + playGroundThickness,
 		puzzleWidth + playGroundThickness,
 		Color::Green));
@@ -93,51 +115,30 @@ void RenderingEngine::Draw(std::string const& formatedGrid, Vector2u const& grid
 	{
 		switch (caseType)
 		{
-		case '#':
-			Draw(RectangleShape(posX, posY, blockSize, blockSize, Color::Black));
-			posX += doubleBlockSize;
-			break;
+			case '#': Draw(RectangleShape(posX, posY, blockSize, blockSize, Color::Black));  break;
+			case '=': Draw(RectangleShape(posX, posY, blockSize, blockSize, Color::Black));  break;
+			case '|': Draw(RectangleShape(posX, posY, blockSize, blockSize, Color::Black));  break;
+			case 'E': Draw(RectangleShape(posX, posY, blockSize, blockSize, Color::Purple)); break;
+			case 'S': Draw(RectangleShape(posX, posY, blockSize, blockSize, Color::Red));    break;
+			case 'X': Draw(RectangleShape(posX, posY, blockSize, blockSize, Color::Blue));   break;
+			default: break;
+		}
 
-		case '=':
-			Draw(RectangleShape(posX, posY, blockSize, blockSize, Color::Black));
-			posX += doubleBlockSize;
-			break;
+		posX += doubleBlockSize;
 
-		case '|':
-			Draw(RectangleShape(posX, posY, blockSize, blockSize, Color::Black));
-			posX += doubleBlockSize;
-			break;
-
-		case 'E':
-			Draw(RectangleShape(posX, posY, blockSize, blockSize, Color::Purple));
-			posX += doubleBlockSize;
-			break;
-
-		case 'S':
-			Draw(RectangleShape(posX, posY, blockSize, blockSize, Color::Red));
-			posX += doubleBlockSize;
-			break;
-
-		case '\n':
+		if(caseType == '\n')
+		{
 			posX = startX;
 			posY += doubleBlockSize;
-			break;
-
-		case 'X':
-			Draw(RectangleShape(posX, posY, blockSize, blockSize, Color::Blue));
-			posX += doubleBlockSize;
-			break;
-
-		case ' ':
-			posX += doubleBlockSize;
-			break;
-
-		default:
-			break;
 		}
 	}
 }
 
+/// \brief	Draws a rectangle on the grid
+/// \param  formatedGrid The grid as a string
+/// \param  gridSize The size of the grid
+/// \param  X The x coordinate of the rectangle
+/// \param  Y The y coordinate of the rectangle
 void RenderingEngine::Draw(std::string const& formatedGrid, Vector2u const& gridSize, USHORT X, USHORT Y)
 {
 	// TODO
