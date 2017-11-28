15c1d701c948774faa0b7613ab937530299fcc18
Darenn Keller <keller.darenn@gmail.com>
2017-11-28 14:45:32 +0100

    Fixed out of range problems.

--- The-Wetness/Source/Generation/GA/GA_Algorithms.cpp
+++ The-Wetness/Source/Generation/GA/GA_Algorithms.cpp
@@ -20,7 +20,7 @@ GA_Algorithms::Chromosome GA_Algorithms::cross(const Chromosome& parentA, const 
 		throw std::invalid_argument("Both parent must have the same size.");
 	}
 	auto child = std::vector<bool>(parentA);
-	auto crossPoint = std::rand() / static_cast<float>(RAND_MAX) * parentB.size();
+	auto crossPoint = std::rand() / static_cast<float>(RAND_MAX) * parentB.size() - 1;
 	for (auto i = crossPoint; i < parentB.size(); ++i)
 	{
 		child[i] = parentB[i];
@@ -33,6 +33,6 @@ const GA_Algorithms::Chromosome& GA_Algorithms::select(const std::vector<std::pa
 	#define COMPARISON [](std::pair<std::vector<bool>, int> a, std::pair<std::vector<bool>, int> b) { return  a.second < b.second; }
 	auto bestFitness = *std::max_element(population.begin(), population.end(), COMPARISON);
 	std::min_element(population.begin(), population.end(), COMPARISON);
-	auto parentIndex = std::rand() / static_cast<float>(RAND_MAX) * population.size();
+	auto parentIndex = std::rand() / static_cast<float>(RAND_MAX) * population.size() - 1;
 	return population[parentIndex].first;
 }
